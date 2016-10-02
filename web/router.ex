8f400a4703723b0729f980521126c64729e2fdec
Abdullah <Abdullah.dahmash@yahoo.com>
2016-10-02 16:10:16 +0300

    Add bearer realm

--- web/router.ex
+++ web/router.ex
@@ -8,7 +8,7 @@ defmodule Peepchat.Router do
   # Authenticated Requests
   pipeline :api_auth do
     plug :accepts, ["json","json-api"]
-    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
   end
 
@@ -24,5 +24,5 @@ defmodule Peepchat.Router do
     pipe_through :api_auth
     get "/user/current", UserController, :current
   end
-  
+
 end
